2490e2d4913fb811d4088dd54988282e06bff820
Paul Walsh <twentytwentyk@live.com>
2019-06-23 13:50:06 -0400

    LinkedLists

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -40,12 +40,87 @@ class LinkedList {
 
     return null;
   }
+  insertAtIndex(index, value, currentNode = this.firstNode, currentIndex = 0) {
+    while (currentIndex < index) {
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+    let newNode = new Node(value);
+    newNode.nextNode = currentNode.nextNode;
+    currentNode.nextNode = newNode;
+    return newNode;
+  }
+  deleteAtIndex(index, currentNode = this.firstNode, currentIndex = 0) {
+    while (currentIndex < index) {
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+    let nodeAfterDeletedNode = currentNode.nextNode.nextNode;
+    currentNode.nextNode = nodeAfterDeletedNode;
+  }
 }
 
 const list1 = new LinkedList(node1);
+console.log(list1);
 console.log(list1.read(3));
 console.log(list1.indexOf("c"));
+console.log(list1.insertAtIndex(2, "hi"));
+console.log(list1.deleteAtIndex(2));
+console.log(list1);
 
+class Node2 {
+  constructor(data, nextNode, prevNode) {
+    this.data = data;
+    this.nextNode = nextNode;
+    this.prevNode = prevNode;
+  }
+}
+class DoublyLinkedList {
+  constructor(firstNode = null, lastNode = null) {
+    this.firstNode = firstNode;
+    this.lastNode = lastNode;
+  }
+  insertAtEnd(value) {
+    let newNode = new Node2(value);
+    if (!this.firstNode) {
+      this.firstNode = newNode;
+      this.lastNode = newNode;
+    } else {
+      newNode.prevNode = this.lastNode;
+      this.lastNode.nextNode = newNode;
+      this.lastNode = newNode;
+    }
+  }
+  removeFromFront() {
+    let removedNode = this.firstNode;
+    this.firstNode = this.firstNode.nextNode;
+    return removedNode;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.queue = new DoublyLinkedList();
+  }
+  enque(value) {
+    this.queue.insertAtEnd(value);
+  }
+  deque() {
+    let removedNode = this.queue.removeFromFront();
+    return removedNode.data;
+  }
+  tail() {
+    return this.queue.lastNode.data;
+  }
+}
+let queue1 = new Queue();
+queue1.enque("1");
+queue1.enque("2");
+queue1.enque("33");
+console.log(queue1);
+queue1.deque();
+console.log(queue1);
+console.log(queue1.tail());
 export const LinkedLists = (
   <div>
     <div>{}</div>
